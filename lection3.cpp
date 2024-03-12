9f2b70bcaa328b5b41d2c6f462fd73953cbfd025
Shuvalova Valeria <114807973+ShuvalovaVD@users.noreply.github.com>
2024-03-12 12:19:59 +0400

    Update lection3.cpp

--- lection3.cpp
+++ lection3.cpp
@@ -41,9 +41,65 @@
 using namespace std;
 
 
+class ODMAS {
+private:
+	int N;
+	int* mas;
+public:
+	ODMAS(int N, int a) {
+		ODMAS::N = N;
+		mas = new int[N];
+		for (int i = 0; i < N; i++) mas[i] = a;
+	}
+	void myprint() {
+		for (int i = 0; i < N; ++i) {
+			cout << mas[i] << " ";
+		}
+		cout << "\n\n";
+	}
+	~ODMAS() {
+		delete[] mas;
+	}
+	friend void mult(ODMAS& m, int k);
+	friend void operator+(ODMAS& a, int b);
+	friend ODMAS operator-(ODMAS& a, int b);
+};
+
+void mult(ODMAS& m, int k) {
+	for (int i = 0; i < m.N; i++) {
+		m.mas[i] = m.mas[i] * k;
+	}
+}
+
+void operator+(ODMAS & a, int b) {  // оператор + не возвращает значение
+	for (int i = 0; i < a.N; ++i) {
+		a.mas[i] += b;
+	}
+}
+
+ODMAS operator-(ODMAS & a, int b) {  // оператор - возвращает значение
+	static ODMAS rez(a.N, 10);  // static нужен, чтобы правильно сработало
+	for (int i = 0; i < a.N; ++i) {
+		rez.mas[i] = a.mas[i] - b;
+	}
+	return rez;
+}
+
+
 int main() {
 	setlocale(LC_ALL, "ru");
 	srand((unsigned)time(NULL));
+
+	ODMAS x1(10, 10);
+	x1.myprint();
+	int k1 = 5;
+	x1 + k1;
+	x1.myprint();
+	// если хотим, чтобы значение возвращалось
+	int k2 = 20;
+	ODMAS x2 = x1 - k2;  // уже новый объект x2
+	x2.myprint();
+	// будет вызываться исключение, но игнорируем его (можно отключить в настройках)
 
 	return 0;
 }
