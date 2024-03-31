80f050b955c89399f454dc6366d8575d3ee0c086
Shuvalova Valeria <114807973+ShuvalovaVD@users.noreply.github.com>
2024-03-31 16:01:59 +0400

    Update lection4.cpp

--- lection4.cpp
+++ lection4.cpp
@@ -6,84 +6,202 @@
 Родительский класс - базовый, дочерний - производный.
 class [имя производного класса] : [право наследование] [имя базового класса]
 Перед именем базового класса указывается право наследования. Право наследования влияет на право доступа к компонентам класса.
-Права наследования:
+Производный класс наследует всё, что есть в базовом классе (всё, что разрешено). Не наследуются конструкторы, деструкторы и переопределённый оператор "=".
+
+Права наследования (базовый класс -> производный класс):
 public:
 public -> public
 protected -> protected
 private -> недоступно
 
+protected:
+public -> protected
+protected -> protected
+private -> недоступно
 
+private:
+public -> private
+protected -> private
+private -> недоступно
+
+Лучше испольщоваться наследование с правом private, предварительно в базовом классе изменив все private на protected, чтобы иметь доступ ко всем компонентам.
+
+При создании объекта производного класса сначала вызываются конструкторы базового класса, а затем конструкторы производного класса. При разрушении объекта 
+сначала вызываются деструкторы производного класса, а затем базового.
 */
 
 #include <iostream>
-#include <math.h>
-#include <algorithm>
-#include <locale.h>
 #include <iomanip>
+#include <stdlib.h>
+#include <locale.h>
 #include <time.h>
 
 using namespace std;
 
-
-class ODMAS {
-private:
+class INTMAS
+{
+protected:
 	int N;
 	int* mas;
 public:
-	ODMAS(int N, int a) {
-		ODMAS::N = N;
+	INTMAS(int n)
+	{
+		N = n;
 		mas = new int[N];
-		for (int i = 0; i < N; i++) mas[i] = a;
+		for (int i = 0; i < N; i++) mas[i] = rand() % 100;
 	}
-	void myprint() {
-		for (int i = 0; i < N; ++i) {
-			cout << mas[i] << " ";
+	void myprint()
+	{
+		cout << endl;
+		for (int i = 0; i < N; i++) cout << setw(3) << mas[i];
+		cout << endl;
+	}
+	int search()
+	{
+		int mn = mas[0];
+		for (int i = 1; i < N; i++) {
+			if (mas[i] < mn) {
+				mn = mas[i];
+			}
 		}
-		cout << "\n\n";
+		return mn;
 	}
-	~ODMAS() {
+	virtual void vir() {
+		cout << "Размер массива: " << N << endl;
+		cout << "Элементы массива: ";
+		for (int i = 0; i < N; i++) cout << setw(3) << mas[i];
+		cout << endl;
+	}
+	friend void delet(INTMAS& mas, int nd);
+	friend void ins(INTMAS& mas, int a, int ind);
+	~INTMAS()
+	{
 		delete[] mas;
 	}
-	friend void mult(ODMAS& m, int k);
-	friend void operator+(ODMAS& a, int b);
-	friend ODMAS operator-(ODMAS& a, int b);
 };
 
-void mult(ODMAS& m, int k) {
-	for (int i = 0; i < m.N; i++) {
-		m.mas[i] = m.mas[i] * k;
+
+void delet(INTMAS& mas, int nd)
+{
+	nd--;
+	for (int i = nd; i < mas.N - 1; i++) {
+		mas.mas[i] = mas.mas[i + 1];
 	}
+	mas.mas[mas.N - 1] = 0;
+	for (int i = 0; i < mas.N - 1; i++) cout << setw(3) << mas.mas[i];
+	cout << endl;
+}
+void ins(INTMAS& mas, int a, int ind)
+{
+	int* nmas;
+	nmas = new int[mas.N + 1];
+	for (int i = 0; i < ind; i++) {
+		nmas[i] = mas.mas[i];
+	}
+	nmas[ind] = a;
+	for (int i = ind; i < mas.N; i++) {
+		nmas[i + 1] = mas.mas[i];
+	}
+	for (int i = 0; i < mas.N + 1; i++) {
+		cout << setw(3) << nmas[i];
+	}
+	cout << endl;
 }
 
-void operator+(ODMAS& a, int b) {  // оператор + не возвращает значение
-	for (int i = 0; i < a.N; ++i) {
-		a.mas[i] += b;
+
+class INTMAS2 : public INTMAS {
+	int summ, pr;
+public:
+	INTMAS2(int n) :INTMAS(n) {
+		summ = 0;
+		pr = 1;
 	}
-}
-
-ODMAS operator-(ODMAS& a, int b) {  // оператор - возвращает значение
-	static ODMAS rez(a.N, 10);  // static нужен, чтобы правильно сработало
-	for (int i = 0; i < a.N; ++i) {
-		rez.mas[i] = a.mas[i] - b;
+	void mysum() {
+		for (int i = 0; i < N; i++) summ += mas[i];
+		cout << "Сумма элементов массива: " << summ << endl;
 	}
-	return rez;
-}
+	void mypr() {
+		for (int i = 0; i < N; i++) pr *= mas[i];
+		cout << "Произведение элементов массива: " << pr << endl;
+	}
+	void search2()
+	{
+		int mx = mas[0];
+		for (int i = 1; i < N; i++) {
+			if (mas[i] > mx && mas[i] % 2 == 0) {
+				mx = mas[i];
+			}
+		}
+		cout << "Максимальный четный элемент массива: " << mx << endl;
+	}
+	void vir() {
+		cout << "Элементы массива: ";
+		for (int i = 0; i < N; i++) cout << setw(3) << mas[i];
+		cout << endl;
+		int maxx = mas[0], minn = mas[0];
+		for (int i = 1; i < N; i++) {
+			if (mas[i] > maxx) {
+				maxx = mas[i];
+			}
+			if (mas[i] < minn) {
+				minn = mas[i];
+			}
+		}
+		cout << "Максимальный элемент массива: " << maxx << endl;
+		cout << "Минимальный элемент массива: " << minn << endl;
+	}
+};
 
-
-int main() {
+int main()
+{
 	setlocale(LC_ALL, "ru");
 	srand((unsigned)time(NULL));
+	cout << "Объект 1" << endl;
+	INTMAS m1(10);
+	m1.myprint();
+	int s;
+	cout << "Какой элемент удалить?  ";
+	cin >> s;
+	cout << "Из массива удалён элемент под номером " << s << endl;
+	delet(m1, s);
 
-	ODMAS x1(10, 10);
-	x1.myprint();
-	int k1 = 5;
-	x1 + k1;
-	x1.myprint();
-	// если хотим, чтобы значение возвращалось
-	int k2 = 20;
-	ODMAS x2 = x1 - k2;  // уже новый объект x2
-	x2.myprint();
-	// будет вызываться исключение, но игнорируем его (можно отключить в настройках)
+	cout << "Объект 2" << endl;
+	INTMAS m2(10);
+	m2.myprint();
+	int a, b;
+	cout << "Какой элемент добавить?  ";
+	cin >> a;
+	cout << "На какое место?  ";
+	cin >> b;
+	b--;
+	cout << "В массив добавлен элемент " << a << endl;
+	ins(m2, a, b);
 
+	cout << "Объект 3" << endl;
+	INTMAS m3(10);
+	m3.myprint();
+	cout << "Минимальный элемент массива: " << m3.search() << endl;
+
+	cout << "------------------------------------------------" << endl;
+
+	// лабораторная 3
+	INTMAS* p;
+
+	cout << "Производный объект: " << endl;
+	INTMAS2 m5(10);
+	m5.myprint();
+	m5.mysum();
+	m5.mypr();
+	p = &m5;
+	cout << "Работа виртуальной функции производного объекта: " << endl;
+	m5.vir();
+	cout << endl;
+	cout << "Базовый объект: " << endl;
+	INTMAS m7(10);
+	m7.myprint();
+	p = &m7;
+	cout << "Работа виртуальной функции базового объекта: " << endl;
+	m7.vir();
+	system("pause");
 	return 0;
 }
